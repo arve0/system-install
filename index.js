55a4e4137c6eb94a619427171991bfa3702e34e7
techno-express <technoexpressnet@gmail.com>
2018-02-13 22:45:50 -0500

    update tests, refactor code,

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var INSTALL_CMD = {
 	port: 'sudo port install',
 	pkgin: 'sudo pkgin install',
 	choco: 'choco install',
-	powershell: "powershell Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))",
+	powershell: "powershell ./installChocolatey.ps1",
 	'apt-get': 'sudo apt-get install',
 	yum: 'sudo yum install',
 	dnf: 'sudo dnf install',
@@ -18,8 +18,7 @@ var INSTALL_CMD = {
 	pacman: 'sudo pacman -S',
 	pkg: 'pkg install',
 	pkg_add: 'pkg_add',
-	crew: 'crew install',
-	test: 'dummy installer'
+	crew: 'crew install'
 };
 
 var PKG_MANAGERS = {
@@ -31,7 +30,31 @@ var PKG_MANAGERS = {
 	test: ['dummy']
 	//netbsd?
 };
+
+function package_manager(reject) {
+    if (!reject) var reject = function (data) { return new Error(data); }
+    var managers = PKG_MANAGERS[process.platform];
+        if (!managers || !managers.length) {
+            return reject(Error('unknown platform \'' + process.platform + '\''));
+        }
     
+        managers = managers.filter(function (mng) {
+            try {
+                // TODO: Optimize?
+                which.sync(mng);
+                return true;
+            } catch (e) {
+                return false;
+            }
+        });
+    
+        if (!managers.length) {
+            return reject(Error('your_package_manager install'));
+        }
+        
+    return INSTALL_CMD[managers[0]].split(' ');    
+}
+
 /**
  * Gets the system packaging install command.
  *
@@ -45,29 +68,11 @@ var PKG_MANAGERS = {
 module.exports = getInstallCmd.packager = getInstallCmd; 
 
     getInstallCmd.packager = function packager() {
-        var managers = PKG_MANAGERS[process.platform];
-        if (!managers || !managers.length) {
-            return new Error('unknown platform \'' + process.platform + '\'');
-        }
-        managers = managers.filter(function (mng) {
-            try {
-                // TODO: Optimize?
-                which.sync(mng);
-                return true;
-            } catch (e) {
-                return false;
-            }
-        });
-        if (!managers.length) {
-            return new Error('your_package_manager install');
-        }   
-
-        var system_installer = INSTALL_CMD[managers[0]].split(' ');  
-        return {
-            needsudo: (system_installer[0]=='sudo') ? true : false,
-            packager: (!system_installer[2]) ? system_installer[0] : system_installer[1],
-            installercommand: INSTALL_CMD[managers[0]]
-            } 
+        var system_installer = package_manager();
+        if (system_installer[0]) return { needsudo: (system_installer[0]=='sudo') ? true : false,
+                                          packager: (!system_installer[2]) ? system_installer[0] : system_installer[1],
+                                  installercommand: system_installer.join(' ') }
+        else return new Error(system_installer);
     };  
     
 /**
@@ -78,30 +83,11 @@ module.exports = getInstallCmd.packager = getInstallCmd;
  *                   Defaults to 'your_package_manager install' if no package manager is found.
  * @throws Throws if `process.platform` is none of darwin, freebsd, linux, sunos or win32.
  */
-    getInstallCmd.install = function install(application) {
+    getInstallCmd.installer = function installer(application) {
       return new Promise(function (resolve, reject) {
-        if (!application) return reject("Error: No package, application name missing.");    
-    
-        var managers = PKG_MANAGERS[process.platform];
-        if (!managers || !managers.length) {
-            return reject('unknown platform \'' + process.platform + '\'');
-        }
-    
-        managers = managers.filter(function (mng) {
-            try {
-                // TODO: Optimize?
-                which.sync(mng);
-                return true;
-            } catch (e) {
-                return false;
-            }
-        });
-    
-        if (!managers.length) {
-            return reject('your_package_manager install');
-        }   
-
-        var system_installer = INSTALL_CMD[managers[0]].split(' ');
+        if (!application) return reject(Error("Error: No package, application name missing."));  
+        
+        var system_installer = package_manager(reject);
         var cmd = system_installer[0];
         if (system_installer[1]) var args = [ system_installer[1] ];
         if (system_installer[2]) var install = [ system_installer[2] ];
@@ -111,14 +97,32 @@ module.exports = getInstallCmd.packager = getInstallCmd;
         if ((args) && (!install)) distro = args.concat(whattoinstall);
         if ((args) && (install)) distro = args.concat(install).concat(whattoinstall);
         
-        if (cmd!='powershell') {
+        if (cmd!='powershell') { 
             console.log('Running ' + cmd  + ' ' + distro);        
             var result = require('child_process').spawn(cmd, distro, { stdio: 'pipe' });
-            result.on('error', (err) => { return reject(result.error); }); 
+            result.on('error', (err) => { return reject(Error(err)); }); 
             result.stdout.on('data', function(data) { console.log(data.toString()); });
-            result.stderr.on('data', function(data) { console.log(data.toString()); });
-            result.on('close', function(code) { resolve(code) });             
-        }  
+            result.stderr.on('data', function(data) { return reject(Error(data.toString())); });
+            result.on('close', function(code) { return resolve(code) });    
+            result.on('exit', function(code) { return resolve(code) });    
+        } else return reject(Error('No windows package manager installed!'));
+        /*else if (process.platform=='win32') { 
+            var PowerShell = require("powershell");
+            console.log('Download and Install Chocolatey');        
+            var ps = new PowerShell("./installChocolatey.ps1",{ executionpolicy: 'Unrestricted' });
+            ps.on('error',  (err) => { return reject(Error(err)); }); 
+            ps.on('output', function(data) { console.log(data); });
+            ps.on('error-output', function(data) { return reject(Error(data)); });
+            ps.on('end', function(code) { 
+                console.log('Running choco install ' + whattoinstall);        
+                var packageresult = require('child_process').spawn('choco', ['install'].concat(whattoinstall), { stdio: 'pipe' });
+                packageresult.on('error', (err) => { return reject(Error(result.error)); }); 
+                packageresult.stdout.on('data', function(data) { console.log(data.toString()); });
+                packageresult.stderr.on('data', function(data) { return reject(Error(data.toString())); });
+                packageresult.on('close', function(code) { return resolve(code); } ); 
+                packageresult.on('exit', function(code) { return resolve(code) });    
+            });                    
+        }*/
       });        
     }
     
--- test/test.js
+++ test/test.js
@@ -1,23 +1,8 @@
 var expect = require('chai').expect;
 var system = require('../index.js').packager;
+var installer = require('../index.js').installer;
 
-describe('Method: `system-installer()` for platform set to `win32`', function() {
-    
-    before(function() {
-        // save original process.platform
-        this.originalPlatform = Object.getOwnPropertyDescriptor(process, 'platform');
-
-        // redefine process.platform
-        Object.defineProperty(process, 'platform', {
-            value: 'win32'
-        });
-    });
-
-    after(function() {
-        // restore original process.platfork
-        Object.defineProperty(process, 'platform', this.originalPlatform);
-    });
-  
+describe('Method: `packager`', function() {    
     it('should return an object', function (done) {
         expect(system()).to.be.an('object');
         done();
@@ -27,29 +12,20 @@ describe('Method: `system-installer()` for platform set to `win32`', function() 
         var i = system();
         expect(i.needsudo).to.be.a('boolean');
         expect(i.packager).to.be.a('string');
+        console.log(i.installercommand);
         expect(i.installercommand).to.be.a('string');
         done();
-    });
-    
+    });    
 });    
 	
-describe('Method: `system-installer()` for platform set to `other`', function() {
-    
-    before(function() {
-        // save original process.platform
-        this.originalPlatform = Object.getOwnPropertyDescriptor(process, 'platform');
+describe('Method: `packager` for platform set to `other`', function() {
+    // save original process.platform    
+    before(function() { this.originalPlatform = Object.getOwnPropertyDescriptor(process, 'platform');
+    // redefine process.platform
+        Object.defineProperty(process, 'platform', { value: 'other' }); });
+    // restore original process.platform
+    after(function() { Object.defineProperty(process, 'platform', this.originalPlatform); });
 
-        // redefine process.platform
-        Object.defineProperty(process, 'platform', {
-            value: 'other'
-        });
-    });
-
-    after(function() {
-        // restore original process.platfork
-        Object.defineProperty(process, 'platform', this.originalPlatform);
-    });
-  
     it('should return an error for unknown platform', function (done) {
         expect(system()).to.be.an.instanceof(Error);
         done();
@@ -57,22 +33,13 @@ describe('Method: `system-installer()` for platform set to `other`', function() 
 });    
 	
 	
-describe('Method: `system-installer()` for platform set to `test`', function() {
-    
-    before(function() {
-        // save original process.platform
-        this.originalPlatform = Object.getOwnPropertyDescriptor(process, 'platform');
-
-        // redefine process.platform
-        Object.defineProperty(process, 'platform', {
-            value: 'test'
-        });
-    });
-
-    after(function() {
-        // restore original process.platfork
-        Object.defineProperty(process, 'platform', this.originalPlatform);
-    });
+describe('Method: `packager` for platform set to `test`', function() {    
+    // save original process.platform    
+    before(function() { this.originalPlatform = Object.getOwnPropertyDescriptor(process, 'platform');
+    // redefine process.platform
+        Object.defineProperty(process, 'platform', { value: 'test' }); });
+    // restore original process.platform
+    after(function() { Object.defineProperty(process, 'platform', this.originalPlatform); });
         
     it('should return an error for no package manager found', function (done) {
         expect(system()).to.be.an.instanceof(Error);
@@ -80,20 +47,84 @@ describe('Method: `system-installer()` for platform set to `test`', function() {
     });    	
 });    
 	
-describe('Method: `system-installer(application)` for platform set to `win32`', function() {
-    
-    before(function() {
-        // save original process.platform
-        this.originalPlatform = Object.getOwnPropertyDescriptor(process, 'platform');
-
-        // redefine process.platform
-        Object.defineProperty(process, 'platform', {
-            value: 'win32'
+describe('Method: `installer`', function() {
+    it('should return an error for no package, application name missing', function (done) {
+        installer(null)
+        .catch(function(err) {
+            expect(err).to.be.an.instanceof(Error);
+            done();            
         });
-    });
-
-    after(function() {
-        // restore original process.platfork
-        Object.defineProperty(process, 'platform', this.originalPlatform);
-    });	
+    });    	
 });
+
+describe('Method: `installer` for platform set to `other`', function() {
+    // save original process.platform    
+    before(function() { this.originalPlatform = Object.getOwnPropertyDescriptor(process, 'platform');
+    // redefine process.platform
+        Object.defineProperty(process, 'platform', { value: 'other' }); });
+    // restore original process.platform
+    after(function() { Object.defineProperty(process, 'platform', this.originalPlatform); });
+   
+    it('should return an error for unknown platform', function (done) {
+        installer('winrar')
+        .catch(function(err) {
+            expect(err).to.be.an.instanceof(Error);
+            done();           
+        });
+    });        
+});
+
+describe('Method: `installer` for platform set to `test`', function() {
+    // save original process.platform    
+    before(function() { this.originalPlatform = Object.getOwnPropertyDescriptor(process, 'platform');
+    // redefine process.platform
+        Object.defineProperty(process, 'platform', { value: 'test' }); });
+    // restore original process.platform
+    after(function() { Object.defineProperty(process, 'platform', this.originalPlatform); });
+        
+    it('should return an error for no package manager found', function (done) {
+        installer('winrar')
+        .catch(function(err) {
+            expect(err).to.be.an.instanceof(Error);
+            done();            
+        });
+    });  
+});
+
+describe('Method: `installer` install package `???`', function() {
+    it('should return an error for any issues installing package', function (done) {
+        installer('???')
+        .catch(function(err) {
+            expect(err).to.be.an.instanceof(Error);
+            done();            
+        });
+    });    	
+});
+
+describe('Method: `installer` install package `vim`', function() {
+    it('should return on successful install or errors if windows', function (done) {
+        installer('vim')
+        .then(function() {
+            done();          
+        })
+        .catch(function(err) {
+            expect(err).to.be.an.instanceof(Error);
+            done();            
+        }); 
+    });    	
+});
+
+describe('Method: `installer` install packages `unzip` and `nano`', function() {
+    it('should return on successful install of multiple packages or print error log if windows platforn', function (done) {
+        installer(['unzip','nano'])
+        .then(function() {
+            done();          
+        })
+        .catch(function(err) {
+            console.log(err);
+            expect(err).to.be.an.instanceof(Error);
+            done();            
+        }); 
+    });    	
+});
+
